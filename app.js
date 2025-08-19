a1a521d5a7cf607f31465143775a59742ff7a976
M Ahsan Sandhu <ahsanhd@outlook.com>
2025-08-19 18:13:07 +0500

    first Node JS commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// const EventEmitter = require('event');
+
+// const customEmitter = new EventEmitter;
+
+const count = 10;
+
+if (count < 10) {
+  console.info("Small number");
+} else {
+  console.info("It is a large number");
+}
+
+console.log("HELLO WORLDDDD! THis is my start of Node JS journey");
