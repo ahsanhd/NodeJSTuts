25a1589e32b6a090a23370acfbba6d5e21f8a6f4
M Ahsan Sandhu <ahsanhd@outlook.com>
2025-08-20 12:42:03 +0500

    general globals in JS and node JS

---
+++ 02-globals.js
@@ -0,0 +1,12 @@
+console.log(__dirname);
+/* Global -- NO WINDOWS in Node JS */
+
+/* __dirname */
+/* __filename */
+/* process --- info about env where program is executed */
+/* require ---- function to use modules (CommonJS) */
+/* module ----  info about current module (file)*/
+
+setInterval(() => {
+  console.log("HELLO WORLD!!");
+}, 1000);
